ff0c95efadc625054db58c72e9d107464c02439b
Ruchira1357 <susarruchira1@gmail.com>
2023-08-18 19:03:45 +0530

    vpc api changed

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform"
-    key = "main"
+    bucket = "acent-tcw-18-8-23"
+    key = "main/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
